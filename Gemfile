979253ea989a3c2428e2d0ec341575aa71fd5105
farhanprasla10 <farhan.prasla31@gmail.com>
2019-05-19 21:09:24 +0000

    Styling

--- Gemfile
+++ Gemfile
@@ -79,3 +79,6 @@ end
 gem 'bootstrap', '4.0.0.alpha6'
 gem 'simple_form'
 gem 'devise'
+gem 'carrierwave', '~> 1.0'
+gem "figaro"
+gem "fog-aws"
--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -28,6 +28,7 @@ class GramsController < ApplicationController
   end
 
   def index
+     @grams = Gram.all
   end
 
   def show
@@ -53,14 +54,7 @@ class GramsController < ApplicationController
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
-  def render_not_found
-    render plain: 'Not Found :(', status: :not_found
-  end
-
-   def render_not_found(status=:not_found)
-    render plain: "#{status.to_s.titleize} :(", status: status
-  end
 end
